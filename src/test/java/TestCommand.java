a79dfc9dd716b9d887acfe0a1c39a8b6264afee0
gsautomationtest <test@example.com>
2022-03-30 10:46:48 -0400

    Dev

--- src/test/java/TestCommand.java
+++ src/test/java/TestCommand.java
@@ -1,19 +1,29 @@
-import com.automationanywhere.botcommand.GetTable;
-import mmarquee.automation.AutomationException;
-import mmarquee.automation.controls.DataGrid;
+import com.automationanywhere.botcommand.GetTableCommand;
+import com.automationanywhere.botcommand.data.Value;
+import com.automationanywhere.botcommand.data.impl.TableValue;
+import com.automationanywhere.botcommand.data.model.table.Row;
+import com.automationanywhere.botcommand.data.model.table.Table;
 import org.testng.annotations.Test;
+
+import java.util.List;
+
 @Test
 
 public class TestCommand {
     public void TestGetGrid() throws Exception {
-        GetTable GetTable1 = new GetTable();
+        GetTableCommand TestCommand = new GetTableCommand();
 
-        String WindowTitle = "Advantage - >> Agora Training Area <<";
+        String WindowTitle = "Advantage - >> Agora Production Area <<";
         String RootTabName = "Omnistream Transactions (MSTOSM)";
         String TargetViewportID = "mainDataViewPort";
 
         //eventToNewRelic.PostEvent(key, values);
-        DataGrid ReturnedGrid = GetTable1.FindDataGrid(WindowTitle, TargetViewportID, RootTabName);
-        GetTable1.DataGridToAATable(ReturnedGrid, true);
+        Value<Table> TableValue = new TableValue();
+        TableValue = TestCommand.action("", WindowTitle, RootTabName, TargetViewportID);
+        Table Table = TableValue.get();
+        List<Row> Rows = Table.getRows();
+        for (Row CurrentRow: Rows) {
+            System.out.println(CurrentRow.getValues().get(0));
+        }
     }
 }
